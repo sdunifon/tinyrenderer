6f48db7c73c3b8cac3f8800ec85d718cb6b304d8
Scott Dunifon <sdunifon@gmail.com>
2021-12-02 10:13:12 -0500

    serving wasm files from warp

--- web-server/src/main.rs
+++ web-server/src/main.rs
@@ -2,7 +2,12 @@ use warp::Filter;
 
 #[tokio::main]
 async fn main() {
-    println!("hello world");
-    let hello = warp::path!("hello" / String).map(|name| format!("Hello, {}", name));
-    warp::serve(hello).run(([127, 0, 0, 1], 4040)).await
+    println!("hello world!");
+    // let hello = warp::path!("hello" / String).map(|name| format!("Hello, {}", name));
+    // warp::serve(hello).run(([127, 0, 0, 1], 4040)).await
+    // pretty_env_logger::init();
+
+    warp::serve(warp::fs::dir("./web/dist"))
+        .run(([127, 0, 0, 1], 3030))
+        .await;
 }
